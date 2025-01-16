2a96d59cb0f163813875a0eda9e45d64d82209d9
NorPeK <norpekbusiness@hotmail.com>
2025-01-16 14:01:02 +0300

    tabs navigations implemented

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -11,10 +11,6 @@ export default function Index() {
       }}
     >
       <Text className="font-bold my-10 text-3xl font-rubik">Welcome to ReState</Text>
-      <Link href="/SignIn">Sign In</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/Profile">Profile</Link>
-      <Link href="/properties/[id]">Porperty</Link>
     </View>
   );
 }
