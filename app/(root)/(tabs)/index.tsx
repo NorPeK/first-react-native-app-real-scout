4aab4bb331703d2459f99792fa5b4e13023649b8
NorPeK <norpekbusiness@hotmail.com>
2025-01-19 23:00:31 +0300

    seeding completed

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -4,8 +4,9 @@ import SearchBar from "@/components/SearchBar";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
 import { useGlobalContext } from "@/lib/global-provider";
+import seed from "@/lib/seed";
 import { Link } from "expo-router";
-import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { Button, FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
@@ -47,7 +48,7 @@ export default function Index() {
           </View>
 
           <FlatList
-            data={[1, 2, 3, 4]}
+            data={[5,6,7]}
             renderItem={({ item }) => <FeaturedCard />}
             keyExtractor={(item) => item.toString()}
             horizontal
